b59ee257b1932a7782432905640a2ab92ce6f346
Aspectious <griffin.siebert14@outlook.com>
2022-11-21 18:37:47 -0500

    Added "Actions" Panel to dashboard.

--- client/js/index.js
+++ client/js/index.js
@@ -4,5 +4,15 @@ fetch(new Request("/request?q=reqissuesunresolved&reqissuesresolved&reqassets&re
     + `<div class="dashboard-statistic">Items Asseted:<span style="float:right">${data.reqassets.length}</span></div>`
     + `<div class="dashboard-statistic">Issue Count:<span style="float:right">${data.reqissuesall.length}</span></div>`
     + `<div class="dashboard-statistic">Unresolved Issues:<span style="float:right">${data.reqissuesunresolved.length}</span></div>`
-    + `<div class="dashboard-statistic">Resolved Issues:<span style="float:right">${data.reqissuesresolved.length}</span></div>`
+    + `<div class="dashboard-statistic">Resolved Issues:<span style="float:right">${data.reqissuesall.length-data.reqissuesunresolved.length}</span></div>` 
+})})
+var displayname
+fetch(new Request("/request?q=currentuser")).then(a => {a.text().then(b=>{
+    data = JSON.parse(b);
+    data.userinfo.forEach(element => {
+        if (element.Username.toLowerCase() == getCookie("account")) {
+            displayname = element.Displayname;
+            document.getElementById("title").innerHTML = `Dashboard<div style="font-size:12pt;margin:0">Welcome back, ${displayname}</div>`
+        }
+    });
 })})
