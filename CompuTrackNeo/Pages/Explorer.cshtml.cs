6652e3ecbb7d1737ac3ae753e96635ce527175bc
Aspectious <griffin.siebert14@outlook.com>
2023-02-16 21:52:10 -0500

    Visual Studio Upload 1

---
+++ CompuTrackNeo/Pages/Explorer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompuTrackNeo.Pages
+{
+    public class ExplorerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CompuTrackNeo/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+	.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+
+builder.Services.AddAuthorization(options =>
+{
+	// By default, all incoming requests will be authorized according to the default policy.
+	options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages()
+	.AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
